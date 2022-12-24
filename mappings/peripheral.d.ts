472bea1024810dcb424ce0cc4923b31aecd80b6c
Your Name <you@example.com>
2022-12-24 01:40:55 -0800

    Add placeholder documentation

--- mappings/peripheral.d.ts
+++ mappings/peripheral.d.ts
@@ -1,26 +1,94 @@
+/**
+ * Sides because why not
+ */
 type side = "top" | "bottom" | "front" | "back" | "left" | "right";
 
+/**
+ * Representation of the lua table
+ * It's basically the same as a javascript object, so there isnt much to do
+ */
 declare class Table extends Object {
     // TODO: something idk lol
 }
 
+/**
+ * Monitor peripheral
+ * TODO: Move to its own file
+ */
 declare class Monitor extends Table {
     write: (this: void, text: string) => void;
     clearLine: (this: void) => void;
     setCursorPos: (this: void, x: number, y: number) => void;
 }
 
+// List of peripherals and their names as strings
 type Peripheral = Monitor;
 type PeripheralType = "monitor";
 
-declare class peripheral {
-    static getNames: (this: void) => string[];
-    static isPresent: (this: void, name: side | string) => boolean;
-    static getType: (this: void, peripheral: side | string | Peripheral) => (PeripheralType | undefined);
-    static hasType: (this: void, peripheral: side | string | Peripheral, peripheral_type: PeripheralType) => boolean;
-    static getMethods: (this: void, name: side | string) => (string[] | undefined);
-    static getName: (this: void, peripheral: Peripheral) => string;
-    static call: (this: void, name: side | string, method: string, ...arguments: any) => any;
-    static wrap: (this: void, name: side | string) => (Peripheral | undefined);
-    static find: (this: void, type: string, filter?: (name: string, wrapped: Peripheral) => boolean) => Peripheral;
+declare let peripheral: {
+    /**
+     * Get the names of connected peripherals
+     * @returns A string array of the names of all connected peripherals
+     */
+    getNames: (this: void) => string[];
+
+    /**
+     * Check if a peripheral exists
+     * @param name The name or side of the side to check
+     * @returns If the peripheral is present given the name or side
+     */
+    isPresent: (this: void, name: side | string) => boolean;
+
+    /**
+     * 
+     * @param peripheral 
+     * @returns 
+     */
+    getType: (this: void, peripheral: side | string | Peripheral) => (PeripheralType | undefined);
+
+    /**
+     * 
+     * @param peripheral 
+     * @param peripheral_type 
+     * @returns 
+     */
+    hasType: (this: void, peripheral: side | string | Peripheral, peripheral_type: PeripheralType) => boolean;
+
+    /**
+     * 
+     * @param name 
+     * @returns 
+     */
+    getMethods: (this: void, name: side | string) => (string[] | undefined);
+
+    /**
+     * 
+     * @param peripheral 
+     * @returns 
+     */
+    getName: (this: void, peripheral: Peripheral) => string;
+
+    /**
+     * 
+     * @param name 
+     * @param method 
+     * @param arguments 
+     * @returns 
+     */
+    call: (this: void, name: side | string, method: string, ...arguments: any) => any;
+
+    /**
+     * 
+     * @param name 
+     * @returns 
+     */
+    wrap: (this: void, name: side | string) => (Peripheral | undefined);
+
+    /**
+     * 
+     * @param type 
+     * @param filter 
+     * @returns 
+     */
+    find: (this: void, type: string, filter?: (name: string, wrapped: Peripheral) => boolean) => Peripheral;
 }
