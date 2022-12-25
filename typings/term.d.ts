8ea0fd39072377cf72772de9c2343a40fd6d0bd0
Shrecknt <you@example.com>
2022-12-25 00:41:22 -0800

    fixed term functions not including this: void arg

--- typings/term.d.ts
+++ typings/term.d.ts
@@ -2,21 +2,21 @@
 // Does not currently implement methods exclusive to monitors or windows
 
 declare let term: {
-	write: (text: string) => void;
-	blit: (text: string, colors: string, background: string) => void;
-	clear: () => void;
-	clearLine: () => void;
-	getCursorPos: () => LuaMultiReturn<[x: number, y: number]>;
-	setCursorPos: (x: number, y: number) => void;
-	setCursorBlink: (bool: boolean) => void;
-	isColor: () => boolean;
-	getSize: () => LuaMultiReturn<[x: number, y: number]>;
-	scroll: (n: number) => void;
-	redirect: (target: any) => Table;
-	current: () => Table;
-	native: () => Table;
-	setTextColor: (color: number) => void;
-	getTextColor: () => number;
-	setBackgroundColor: (color: number) => void;
-	getBackgroundColor: () => number;
+	write: (this: void, text: string) => void;
+	blit: (this: void, text: string, colors: string, background: string) => void;
+	clear: (this: void) => void;
+	clearLine: (this: void) => void;
+	getCursorPos: (this: void) => LuaMultiReturn<[x: number, y: number]>;
+	setCursorPos: (this: void, x: number, y: number) => void;
+	setCursorBlink: (this: void, bool: boolean) => void;
+	isColor: (this: void) => boolean;
+	getSize: (this: void) => LuaMultiReturn<[x: number, y: number]>;
+	scroll: (this: void, n: number) => void;
+	redirect: (this: void, target: any) => Table;
+	current: (this: void) => Table;
+	native: (this: void) => Table;
+	setTextColor: (this: void, color: number) => void;
+	getTextColor: (this: void) => number;
+	setBackgroundColor: (this: void, color: number) => void;
+	getBackgroundColor: (this: void) => number;
 }
