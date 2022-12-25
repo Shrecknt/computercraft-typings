4b84a86b63f9867b9127aa0c574dcc5e78b7ed20
Your Name <you@example.com>
2022-12-25 00:16:18 -0800

    Implement most term methods

--- mappings/term.d.ts
+++ mappings/term.d.ts
@@ -1,5 +1,22 @@
-// TODO
+// Based on old documentation @ https://computercraft.info/wiki/Term_(API)
+// Does not currently implement methods exclusive to monitors or windows
 
 declare let term: {
-	write: (text: string) => void
+	write: (text: string) => void;
+	blit: (text: string, colors: string, background: string) => void;
+	clear: () => void;
+	clearLine: () => void;
+	getCursorPos: () => LuaMultiReturn<[x: number, y: number]>;
+	setCursorPos: (x: number, y: number) => void;
+	setCursorBlink: (bool: boolean) => void;
+	isColor: () => boolean;
+	getSize: () => LuaMultiReturn<[x: number, y: number]>;
+	scroll: (n: number) => void;
+	redirect: (target: any) => Table;
+	current: () => Table;
+	native: () => Table;
+	setTextColor: (color: number) => void;
+	getTextColor: () => number;
+	setBackgroundColor: (color: number) => void;
+	getBackgroundColor: () => number;
 }
