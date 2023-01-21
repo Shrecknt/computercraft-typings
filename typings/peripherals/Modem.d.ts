dc5d28ad2bf47a035e3c2c40a1ccc8bd27f528f5
Shrecknt <you@example.com>
2023-01-21 11:13:30 -0800

    Add modem methods

--- typings/peripherals/Modem.d.ts
+++ typings/peripherals/Modem.d.ts
@@ -1,6 +1,20 @@
+type Range<F extends number, T extends number> = number;
+
 /**
  * Modem peripheral
  */
 declare interface Modem extends Table {
-    
+    open: (this: void, channel: Range<0, 65535>) => void;
+    isOpen: (this: void, channel: Range<0, 65535>) => boolean;
+    close: (this: void, channel: Range<0, 65535>) => void;
+    closeAll: (this: void) => void;
+    transmit: (this: void, channel: Range<0, 65535>, replyChannel: Range<0, 65535>, payload: any) => void;
+    isWireless: (this: void) => boolean;
+    getNamesRemote: (this: void) => string[];
+    isPresentRemote: (this: void, name: PeripheralType | string) => boolean;
+    getTypeRemote: (this: void, name: PeripheralType | string) => string | null;
+    hasRemoteType: (this: void, name: string, type: PeripheralType | string) => boolean | null;
+    getMethodsRemote: (this: void, name: PeripheralType | string) => string[] | null;
+    callRemote: (this: void, remoteName: PeripheralType | string, method: string, ...arguments: any) => string | any;
+    getNameLocal: (this: void) => string | null;
 }
